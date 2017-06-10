796c5cd147e1cba0ed06af1200a411bb88d83507
Taku-Katayama <taku.katayama@outlook.jp>
2017-06-10 18:16:28 +0900

    add function

--- src/beego_move_pwm.cpp
+++ src/beego_move_pwm.cpp
@@ -1,42 +1,82 @@
 #include <runCtrl.h>
 #include <chrono>
 #include <iostream>
+#include <string>
+#include <fstream> // file I/O
 
-void move_process();
-void measure_time();
-
-void move_process()
+namespace agv
 {
-    // Connect beego
-    RunCtrl run;
-    const char *beego_port = "COM5"; // port number
-    run.connect(beego_port);
-
-    // Setting beego
-    enum{ MOTOR_ID_LEFT = 1, MOTOR_ID_RIGHT = 0 }; // motor id
-    run.setWheelVel(MOTOR_ID_LEFT, 20);
-    run.setWheelVel(MOTOR_ID_RIGHT, -20);
-
-    run.setMotorPwm(MOTOR_ID_LEFT, 100);
-    run.setMotorPwm(MOTOR_ID_RIGHT, 100);
+    class robot
+    {
+    private:
+        RunCtrl run;
+        const char *beego_port = "COM4"; //port number
+        enum
+        {
+            MOTOR_ID_LEFT = 1, MOTOR_ID_RIGHT = 0
+        }; // motor id
+        int beego_wheel_velocity = -20;
+        char beego_pwm = 200;
+    public:
+        void init();
+        void move();
+        double time();
+        void end();
+    };
 }
 
- inline void measure_time()
+void agv::robot::init()
+{
+    run.connect(beego_port);
+    run.setWheelVel(MOTOR_ID_LEFT, beego_wheel_velocity);
+    run.setWheelVel(MOTOR_ID_RIGHT, beego_wheel_velocity);
+}
+
+void agv::robot::move()
+{
+    run.setMotorPwm(MOTOR_ID_LEFT, beego_pwm);
+    run.setMotorPwm(MOTOR_ID_RIGHT, beego_pwm);
+}
+
+double agv::robot::time()
 {
     std::chrono::system_clock::time_point start, end;
     start = std::chrono::system_clock::now();
-    move_process();
+    agv::robot::move();
     end = std::chrono::system_clock::now();
     double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    std::cout << "time: " << elapsed << "[ms]" << std::flush;
+    std::cout << "time: " << elapsed << "[ms]" << std::endl;
+    return elapsed;
+}
+
+void agv::robot::end()
+{
+    run.stop();
+    run.disconnect();
 }
 
 int main(int argc, char **argv)
 {
-    //move_process();
-    for (int i = 0; i < 6; i++)
+    // Output file
+    std::string filename = "sampling_time.txt";
+    std::ofstream writing_file;
+    writing_file.open(filename, std::ios::out);
+
+    std::cout << "Writing " << filename << "..." << std::endl;
+    writing_file << "time[ms]" << std::endl;
+
+    double time;
+
+    agv::robot beego;
+    beego.init();
+
+    for (int i = 0; i < 60; i++)
     {
-        measure_time();
+        //beego.move();
+        time = beego.time();
+        writing_file << time << std::endl;
     }
+    
+    beego.end();
     return 0;
 }
